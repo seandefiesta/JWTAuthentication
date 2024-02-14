bac6526c0e44dddca30287a472fc743f69f737cb
Sean Defiesta <seandefiesta03@gmail.com>
2024-02-15 04:19:25 +0800

    Add project files.

---
+++ JWTAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,132 @@
+﻿using JWTAuthentication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserResponseDto req)
+        {
+            string passwordHash
+                = BCrypt.Net.BCrypt.HashPassword(req.Password);
+
+            user.Username = req.Username;
+            user.PasswordHash = passwordHash;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserResponseDto req)
+        {
+            if (user.Username != req.Username)
+            {
+                return BadRequest("Wrong username");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(req.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = CreateToken(user);
+
+            var refreshToken = GenerateRefreshToken();
+            SetRefreshToken(refreshToken);
+
+
+            return Ok(token);
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<ActionResult<string>> RefreshToken()
+        {
+            var refreshToken = Request.Cookies["refreshToken"];
+            if (!user.RefreshToken.Equals(refreshToken))
+            {
+                return Unauthorized("Invalid Refresh Token");
+            }
+            else if (user.TokenExpires < DateTime.Now)
+            {
+                return Unauthorized("Token expired");
+            }
+
+            string token = CreateToken(user);
+            var newRefreshToken = GenerateRefreshToken();
+            SetRefreshToken(newRefreshToken);
+
+            return Ok(token);
+        }
+
+        private RefreshToken GenerateRefreshToken()
+        {
+            var refreshToken = new RefreshToken
+            { 
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                Expires = DateTime.Now.AddMinutes(5)
+            };
+
+            return refreshToken;
+        }
+
+        private void SetRefreshToken(RefreshToken newRefreshToken)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = newRefreshToken.Expires,
+            };
+
+            Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieOptions);
+
+            user.RefreshToken = newRefreshToken.Token;
+            user.TokenCreated = newRefreshToken.Created;
+            user.TokenExpires = newRefreshToken.Expires;
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(5),
+                    signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
---
+++ JWTAuthentication/Models/UserResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTAuthentication.Models
+{
+    public class UserResponseDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
